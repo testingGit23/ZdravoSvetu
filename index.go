aaab214d2adcbe9e81d8375d97f0230eda0a5abc
DinoShambar <dinoshambar@gmail.com>
2018-07-18 16:53:43 +0200

    Izbrishani site fajlovi so main(), dodaden nov index

---
+++ index.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func printaj() {
+	//Da se napravat fukncii kako fmt.Println(funkcijaA())
+	//Dino
+	//Gligor
+	//Darko
+	fmt.Println("example")
+}
+
+func main() {
+	printaj()
+}
