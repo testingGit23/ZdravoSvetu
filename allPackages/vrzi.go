6dfc399d7289e8ae44f8e9c171ea1e11079baf46
DinoShambar <dinoshambar@gmail.com>
2018-07-19 10:34:10 +0200

    Sredeni site packages vo eden package

---
+++ allPackages/vrzi.go
@@ -0,0 +1,17 @@
+package importall
+
+import "./pkg1"
+import "./pkg2"
+import "./pkg3"
+
+func Soberi(a, b int) int {
+	return soberiDvaBroja.SoberiDvaBroja(a, b)
+}
+
+func Mnozhi(a, b int) int {
+	return mnozi.MnoziDvaBroja(a, b)
+}
+
+func Deli(a, b int) int {
+	return dzalepackage.DeliDvaBroja(a, b)
+}
--- index.go
+++ index.go
@@ -1,17 +1,14 @@
 package main
 
 import "fmt"
-import "./pkg"
-import "./pkg2"
-import "./pkg3"
+import . "./allPackages"
 
 func printaj() {
 	//Da se napravat fukncii kako fmt.Println(funkcijaA())
-	fmt.Println(soberiDvaBroja.SoberiDvaBroja(5, 10))
-	fmt.Println(mnozi.MnoziDvaBroja(5, 10))
-	fmt.Println(dzalepackage.DeliDvaBroja(100, 5))
+	fmt.Println(Soberi(5, 10))
+	fmt.Println(Mnozhi(5, 10))
+	fmt.Println(Deli(100, 5))
 	fmt.Println("example")
-	fmt.Println("Djale")
 }
 
 func main() {
