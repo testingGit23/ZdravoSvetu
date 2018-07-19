21afe902ffb21ff19d0c3c730ad3fe65a6c51b37
Gligor23 <glgudjevski@gmail.com>
2018-07-19 09:50:32 +0200

    asff

--- index.go
+++ index.go
@@ -1,12 +1,14 @@
 package main
 
 import "fmt"
-import osnovniFunkcii "../ZdravoSvetu/pkg"
+import "../ZdravoSvetu/pkg"
+import "../ZdravoSvetu/pkg2"
 
 func printaj() {
 	//Da se napravat fukncii kako fmt.Println(funkcijaA())
-	fmt.Println(osnovniFunkcii.SoberiDvaBroja(5, 10))
+	fmt.Println(soberiDvaBroja.SoberiDvaBroja(5, 10))
 	//Gligor aj utre
+	fmt.Println(mnozi.MnoziDvaBroja(5, 10))
 	//Darko
 	fmt.Println("example")
 }
